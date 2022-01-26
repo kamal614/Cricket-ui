ed141f25aa2635c8be50ce65812dcd8da1218380
kamal614 <kamalkishorkumar614@gmail.com>
2022-01-26 12:09:33 +0530

    Slider started

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -7,18 +7,18 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
-          centerTitle: true,
-          leading: Icon(Icons.menu),
-          actions: [Icon(Icons.search)],
-        ),
-        body: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: true,
+        leading: Icon(Icons.menu),
+        actions: [Icon(Icons.search)],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           children: [
             Padding(
-              padding: EdgeInsets.fromLTRB(10, 15, 0, 0),
+              padding: EdgeInsets.fromLTRB(10, 15, 0, 10),
               child: Row(
                 children: [
                   Text(
@@ -28,9 +28,118 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(
-              height: 100,
-            ),
+            CarouselSlider(
+                items: [1, 2, 3].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 10),
+                                  child: Text(
+                                    "3rd Match, ODI - Zimbabwe tour of Ireland, 2021",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Image.asset("assets/images/zim.png",
+                                            height: 40, width: 40),
+                                        Text("Zimbabwe")
+                                      ],
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          "150/10",
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          "20.4 Ov.",
+                                          style: TextStyle(
+                                              fontSize: 12, color: Colors.grey),
+                                        )
+                                      ],
+                                    ),
+                                    Image.asset(
+                                      "assets/images/vs.png",
+                                      height: 30,
+                                      width: 30,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: const [
+                                        Text(
+                                          "150/10",
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Text(
+                                          "20.4 Ov.",
+                                          style: TextStyle(
+                                              fontSize: 13, color: Colors.grey),
+                                        )
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Image.asset("assets/images/ire.png",
+                                            height: 40, width: 40),
+                                        Text("Ireland")
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "IRE Won by 3 Wickets(DLS)",
+                                    style: TextStyle(
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ));
+                    },
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 200.0, viewportFraction: 0.8,
+                  initialPage: 0,
+
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 5),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  // onPageChanged: callbackFunction,
+                  scrollDirection: Axis.horizontal,
+                )),
             Padding(
                 padding: EdgeInsets.fromLTRB(10, 15, 10, 10),
                 child: Row(
@@ -69,43 +178,43 @@ class HomeScreen extends StatelessWidget {
                 )),
           ],
         ),
-        bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home, color: Colors.blue, size: 30),
-                  title: Text('Home'),
-                  backgroundColor: Colors.white),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.circle, color: Colors.grey, size: 30),
-                  title: Text('Matches'),
-                  backgroundColor: Colors.white),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.network_check, color: Colors.grey, size: 30),
-                title: Text('Live'),
-                backgroundColor: Colors.white,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.settings,
-                  color: Colors.grey,
-                  size: 30,
-                ),
-                title: Text('Settings'),
-                backgroundColor: Colors.white,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Colors.grey, size: 30),
-                title: Text('Account'),
-                backgroundColor: Colors.white,
-              ),
-            ],
-            type: BottomNavigationBarType.shifting,
-            currentIndex: 0,
-            selectedItemColor: Colors.black,
-            iconSize: 40,
-            // onTap: _onItemTapped,
-            elevation: 5),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home, color: Colors.blue, size: 30),
+                title: Text('Home'),
+                backgroundColor: Colors.white),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.circle, color: Colors.grey, size: 30),
+                title: Text('Matches'),
+                backgroundColor: Colors.white),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.network_check, color: Colors.grey, size: 30),
+              title: Text('Live'),
+              backgroundColor: Colors.white,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.grey,
+                size: 30,
+              ),
+              title: Text('Settings'),
+              backgroundColor: Colors.white,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.grey, size: 30),
+              title: Text('Account'),
+              backgroundColor: Colors.white,
+            ),
+          ],
+          type: BottomNavigationBarType.shifting,
+          currentIndex: 0,
+          selectedItemColor: Colors.black,
+          iconSize: 40,
+          // onTap: _onItemTapped,
+          elevation: 5),
     );
   }
 }
